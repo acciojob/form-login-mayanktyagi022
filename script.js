4e8288bca6124db19fb7881e49ca7d8cf6265165
mayanktyagi022 <mayank.tyagiy3k@gmail.com>
2025-10-28 18:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let sub=document.getElementById('submit').value;
+sub.onclick=getFormvalue;
 function getFormvalue() {
     //Write your code here
-
+	let name=document.getElementById('fname').value;
+	let lname=document.getElementById('lname').value;
+	alert(name+lname);
 }
